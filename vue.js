ea23a25103d29d68da9cbf7364bbc54783f4493b
Alex Yeung <foray1010@users.noreply.github.com>
2016-09-04 18:28:47 +0800

    3.2.4: vue: Add vue to resolver && Allow use class instead of className in jsx

--- vue.js
+++ vue.js
@@ -7,9 +7,21 @@ module.exports = extendConfig({
     'eslint-config-foray1010/react'
   ],
   settings: {
-    'import/extensions': ['.js', '.vue'],
+    'import/extensions': ['.js', '.jsx', '.vue'],
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.vue', '.json']
+      }
+    },
     react: {
       pragma: 'h'
     }
+  },
+  rules: {
+    'react/jsx-filename-extension': [2, {
+      extensions: ['.jsx', '.vue']
+    }],
+    // allow use `class` instead of `className`
+    'react/no-unknown-property': 0
   }
 })
