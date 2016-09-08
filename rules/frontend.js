f996c4189b9598efc9f1d4feafab8f9880825686
foray1010 <foray1010@users.noreply.github.com>
2016-09-08 22:13:41 +0800

    3.4.1: Disable class-methods-use-this, jsx-a11y/no-static-element-interactions && Relax react/forbid-prop-types

--- rules/frontend.js
+++ rules/frontend.js
@@ -14,5 +14,15 @@ module.exports = {
   globals: {
     document: false,
     window: false
+  },
+  rules: {
+    // sometime we may just want to apply mouseDown event to whole webpage to disable right click
+    // this rule will block us from doing that
+    'jsx-a11y/no-static-element-interactions': 'off',
+
+    // allow 'object', it is very annoying to define the object structure every time
+    'react/forbid-prop-types': ['error', {
+      forbid: ['any', 'array']
+    }]
   }
 }
