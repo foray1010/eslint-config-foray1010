3afca8ca39bde13a0bf47df586bbf8e038a310c6
Alex Yeung <foray1010@users.noreply.github.com>
2016-09-22 17:08:56 +0800

    Clean up old rules && Add comments to decribe the config of rules

--- react.js
+++ react.js
@@ -17,6 +17,19 @@ module.exports = extendConfig({
     /*+++++++++++++++++++
      + Stylistic Issues +
      +++++++++++++++++++*/
-    'jsx-quotes': ['error', 'prefer-single']
+    // I prefer to use same style for all string
+    'jsx-quotes': ['error', 'prefer-single'],
+
+    /*++++++++++++++++
+     + Other plugins +
+     ++++++++++++++++*/
+    // sometime we may just want to apply mouseDown event to whole webpage to disable right click
+    // this rule will block us from doing that
+    'jsx-a11y/no-static-element-interactions': 'off',
+
+    // allow 'object', it is very annoying to define the object structure every time
+    'react/forbid-prop-types': ['error', {
+      forbid: ['any', 'array']
+    }]
   }
 })
