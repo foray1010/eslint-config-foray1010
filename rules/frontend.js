6a71afd409075f6210c576ca8c7b74bfe8e5a609
foray1010 <foray1010@users.noreply.github.com>
2017-12-04 21:42:25 +0800

    remove unused rules & html support

--- rules/frontend.js
+++ rules/frontend.js
@@ -4,11 +4,6 @@ module.exports = {
   env: {
     webextensions: true
   },
-  plugins: ['html'],
-  settings: {
-    'html/indent': '0',
-    'html/report-bad-indent': 'error'
-  },
   globals: {
     document: false,
     window: false
