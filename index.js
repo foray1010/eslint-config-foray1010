d616114024d1164309a6700a0bfdebc1a5156e76
Alex Yeung <foray1010@users.noreply.github.com>
2018-03-27 21:39:27 +0800

    refactor: extract react rules to rules/react and rules/es-modules

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ module.exports = extendsConfig({
   extends: [
     'eslint-config-airbnb/base',
     'eslint-config-foray1010/rules/main',
-    'eslint-config-foray1010/rules/ramda',
-    'eslint-config-foray1010/rules/unit-test'
+    'eslint-config-foray1010/rules/unit-test',
+    'eslint-config-foray1010/rules/ramda'
   ],
   parserOptions: {
     ecmaFeatures: {
       // if you want to use jsx, extends "foray1010/react"
       jsx: false
     },
-    // we use require() instead of `import ... from ...`
+    // latest node LTS doesn't support es6 module yet
     sourceType: 'script'
   },
   rules: {
