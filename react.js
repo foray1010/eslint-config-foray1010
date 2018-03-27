9795af4b286f9f4c92c5918053225292a7aa9cc4
foray1010 <foray1010@users.noreply.github.com>
2018-03-27 21:50:18 +0800

    feat: add flow to react preset

--- react.js
+++ react.js
@@ -6,11 +6,12 @@ module.exports = extendsConfig({
   extends: [
     'eslint-config-airbnb/base',
     'eslint-config-foray1010/rules/main',
-    'eslint-config-foray1010/rules/react',
     'eslint-config-foray1010/rules/frontend',
+    'eslint-config-foray1010/rules/react',
     'eslint-config-foray1010/rules/es-modules',
     'eslint-config-foray1010/rules/unit-test',
     'eslint-config-foray1010/rules/babel',
+    'eslint-config-foray1010/rules/flow',
     'eslint-config-foray1010/rules/ramda'
   ]
 })
