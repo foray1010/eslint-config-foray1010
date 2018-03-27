357416cafdbc274f3a05860ce946baeb36575c50
foray1010 <foray1010@users.noreply.github.com>
2018-03-27 22:15:38 +0800

    refactor: use reduce to generate babel rules

--- rules/babel.js
+++ rules/babel.js
@@ -8,12 +8,15 @@ const baseConfig = extendsConfig({
 
 // as these rules do not work with babel-eslint
 // using eslint-plugin-babel to deal with these rules
-const migratedRules = {}
 const migrateRuleNames = ['new-cap', 'no-invalid-this', 'object-curly-spacing', 'semi']
-for (const migrateRuleName of migrateRuleNames) {
-  migratedRules[migrateRuleName] = 'off'
-  migratedRules[`babel/${migrateRuleName}`] = baseConfig.rules[migrateRuleName]
-}
+const migratedRules = migrateRuleNames.reduce(
+  (acc, migrateRuleName) => ({
+    ...acc,
+    [migrateRuleName]: 'off',
+    [`babel/${migrateRuleName}`]: baseConfig.rules[migrateRuleName]
+  }),
+  {}
+)
 
 module.exports = {
   parser: 'babel-eslint',
