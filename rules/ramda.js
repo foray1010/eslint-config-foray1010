989202f88f652ac26bd30edf571ea32937daa1d9
foray1010 <foray1010@users.noreply.github.com>
2018-03-30 21:51:57 +0800

    feat: enable more ramda rules

--- rules/ramda.js
+++ rules/ramda.js
@@ -2,5 +2,13 @@
 
 module.exports = {
   extends: 'plugin:ramda/recommended',
-  plugins: ['eslint-plugin-ramda']
+  plugins: ['eslint-plugin-ramda'],
+  rules: {
+    'ramda/always-simplification': 'error',
+    'ramda/compose-simplification': 'error',
+    'ramda/eq-by-simplification': 'error',
+    'ramda/pipe-simplification': 'error',
+    'ramda/prefer-both-either': 'error',
+    'ramda/prefer-complement': 'error'
+  }
 }
