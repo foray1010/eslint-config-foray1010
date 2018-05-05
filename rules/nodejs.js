9662ac5d874e23e566b2a4cb82982e64d4ea8ec5
foray1010 <foray1010@users.noreply.github.com>
2018-05-05 16:42:16 +0800

    feat: use eslint-plugin-node to lint unsupported feature in node

--- rules/nodejs.js
+++ rules/nodejs.js
@@ -5,6 +5,8 @@ module.exports = {
     // latest node LTS doesn't support es6 module yet
     sourceType: 'script'
   },
+  extends: 'plugin:node/recommended',
+  plugins: ['eslint-plugin-node'],
   rules: {
     // backend code needs more dynamic structure
     'global-require': 'off'
