22c81b7009cf4f592377467e6e56b45233338e40
Alex Yeung <foray1010@users.noreply.github.com>
2019-01-20 23:15:54 +0800

    feat: use @typescript-eslint as typescript's eslint plugin and parser

--- rules/typescript.js
+++ rules/typescript.js
@@ -1,38 +1,31 @@
 'use strict'
 
 module.exports = {
-  parser: 'typescript-eslint-parser',
-  plugins: ['eslint-plugin-typescript'],
+  parser: '@typescript-eslint/eslint-plugin/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['plugin:@typescript-eslint/recommended'],
   settings: {
     'import/parsers': {
-      'typescript-eslint-parser': ['.ts', '.tsx']
+      '@typescript-eslint/eslint-plugin/parser': ['.ts', '.tsx']
     }
   },
   rules: {
-    'no-undef': 'off',
+    '@typescript-eslint/array-type': ['error', 'generic'],
+    '@typescript-eslint/camelcase': ['error', {properties: 'never'}],
+    '@typescript-eslint/indent': ['error', 2],
+    '@typescript-eslint/member-delimiter-style': [
+      'error',
+      {
+        multiline: {
+          delimiter: 'none'
+        },
+        singleline: {
+          delimiter: 'comma'
+        }
+      }
+    ],
 
-    'typescript/adjacent-overload-signatures': 'error',
-    'typescript/class-name-casing': 'error',
-    'typescript/explicit-function-return-type': ['error', {allowExpressions: true}],
-    'typescript/explicit-member-accessibility': 'error',
-    'typescript/interface-name-prefix': 'off',
-    'typescript/member-delimiter-style': ['error', {delimiter: 'none'}],
-    'typescript/member-naming': 'off',
-    'typescript/member-ordering': 'error',
-    'typescript/no-angle-bracket-type-assertion': 'error',
-    'typescript/no-array-constructor': 'error',
-    'typescript/no-empty-interface': 'error',
-    'typescript/no-explicit-any': 'off',
-    'typescript/no-inferrable-types': 'error',
-    'typescript/no-namespace': 'error',
-    'typescript/no-non-null-assertion': 'error',
-    'typescript/no-parameter-properties': 'error',
-    'typescript/no-triple-slash-reference': 'error',
-    'typescript/no-type-alias': 'off',
-    'typescript/no-unused-vars': 'error',
-    'typescript/no-use-before-define': 'error',
-    'typescript/no-var-requires': 'error',
-    'typescript/prefer-namespace-keyword': 'error',
-    'typescript/type-annotation-spacing': 'error'
+    // not working for types
+    'import/named': 'off'
   }
 }
