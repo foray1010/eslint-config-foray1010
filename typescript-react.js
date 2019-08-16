b6c05aaab149bcaefccc1d0acad2a10cbe29baf8
Alex Young <foray1010@users.noreply.github.com>
2019-08-16 18:20:42 +0800

    feat: add typescript-react preset

---
+++ typescript-react.js
@@ -0,0 +1,14 @@
+'use strict'
+
+module.exports = {
+  extends: [
+    'eslint-config-airbnb/base',
+    'eslint-config-foray1010/rules/base',
+    'eslint-config-foray1010/rules/frontend',
+    'eslint-config-foray1010/rules/react',
+    'eslint-config-foray1010/rules/es-modules',
+    'eslint-config-foray1010/rules/unit-test',
+    'eslint-config-foray1010/rules/typescript',
+    'eslint-config-foray1010/rules/ramda'
+  ].map(require.resolve)
+}
